88fec20edf1bf3d882e14776a8148a7c3eeea82e
nigelb <nigel.blair+github@gmail.com>
2012-04-13 13:13:53 +1000

    Fixed imports of FileSelectorTypes.

--- simpleui/gtk_impl/impl.py
+++ simpleui/gtk_impl/impl.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-from gRefer.ui.chooser_types import FileSelectorTypes
+from simpleui.utils import FileSelectorTypes
 
 
 try:
